051752279b88cac5103a8acc92abbb1e8cb44779
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-11 19:36:01 -0300

    Add an important comment about Array.from()

--- app/range.js
+++ app/range.js
@@ -3,6 +3,14 @@ export const range = (start, end) => {
   /* 
     Create an array of certain length and set the elements within it from
     start value to end value.
+
+    Note: The _ has no special meaning here. The documentation for from()
+    provide different syntaxes that can be used. In this case, we're using
+    the following: Array.from(arrayLike, (element, index) => { ... } ),
+    where the _ replaces "element" in the arrow function.
+
+    More on that in the following link:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
    */
   return Array.from({ length }, (_, idx) => idx + start);
 };
