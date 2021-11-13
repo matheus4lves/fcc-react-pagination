0f744fd4cc91a050487b5f77e56fc5a339ff5349
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-12 23:24:08 -0300

    Config webpack to work with sass

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import "./styles.scss";
 
 if (module.hot) {
   module.hot.accept("./Main.js");
---
+++ app/components/Pagination.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classnames from "classnames";
+
+class className extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <></>;
+  }
+}
+
+export default className;
