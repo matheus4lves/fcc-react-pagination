f5648e196a7250fe7009ef5b39affff73f0e06d6
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-12 23:19:44 -0300

    Move the range function into usePagination
    Besides, to keep the code organized, create a hooks folder and put the
    usePagination hook inside it.

--- app/hooks/usePagination.js
+++ app/hooks/usePagination.js
@@ -1,5 +1,21 @@
 import { useMemo } from "react";
-import range from "./range";
+
+export const range = (start, end) => {
+  let length = end - start + 1;
+  /* 
+    Create an array of certain length and set the elements within it from
+    start value to end value.
+
+    Note: The _ has no special meaning here. The documentation for from()
+    provide different syntaxes that can be used. In this case, we're using
+    the following: Array.from(arrayLike, (element, index) => { ... } ),
+    where the _ replaces "element" in the arrow function.
+
+    More on that in the following link:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+   */
+  return Array.from({ length }, (_, idx) => idx + start);
+};
 
 export const DOTS = "...";
 
