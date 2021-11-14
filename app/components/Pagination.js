e64f789819573ca05dff674efc167e2f767f8b04
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-14 02:01:16 -0300

    Create the Pagination component

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -1,13 +1,73 @@
 import React from "react";
 import classnames from "classnames";
+import { usePagination, DOTS } from "../hooks/usePagination";
+import "../styles/pagination.scss";
 
-class className extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <></>;
-  }
-}
+const Pagination = props => {
+  const { onPageChange, totalCount, siblingCount = 1, currentPage, pageSize, className } = props;
 
-export default className;
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <ul className={classnames("pagination-container", { [className]: className })}>
+      {/* Left navigation arrow */}
+      <li
+        className={classnames("pagination-item", {
+          disabled: currentPage === 1,
+        })}
+        onClick={onPrevious}
+      >
+        <div className="arrow left" />
+      </li>
+      {paginationRange.map((pageNumber, idx) => {
+        // If the pageItem is a DOT, render the DOTS unicode character
+        if (pageNumber === DOTS) {
+          return <li className="pagination-item dots">&#8230;</li>;
+        }
+
+        // Render the Page Pills
+        return (
+          <li
+            key={idx}
+            className={classnames("pagination-item", {
+              selected: pageNumber === currentPage,
+            })}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+      {/*  Right Navigation arrow */}
+      <li
+        className={classnames("pagination-item", {
+          disabled: currentPage === lastPage,
+        })}
+        onClick={onNext}
+      >
+        <div className="arrow right" />
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
